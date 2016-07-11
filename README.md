9acf8c5e7b18ca217b5da57213285196599e229b
Jimmy Chou <cjimmy@ca.ibm.com>
2016-07-10 23:37:16 -0400

    dynamically change class depending on media type

---
+++ README.md
@@ -0,0 +1,13 @@
+This module introduces an extensive social media directive.
+
+Put 'jcMediaBox' as an dependency of your application. 
+Eg. angular.module('yourApp',['jcMediaBox']);
+
+Declare a social media hash object where you would like to use this directive.
+
+For example: 
+If you want to link your facebook and twitter account, feed the following object to the directive:
+{
+    facebook:"",
+    twitter:""
+}
--- directive.js
+++ directive.js
@@ -1,4 +1,35 @@
 angular.module('testDirective',[]).
-directive('jcInputbox',function(){
-    
+directive('jcMediaBox',function(){
+    return {
+        template:"<footer><ul ng-repeat = 'link in links'><li><i ng-class = 'link.class'>{{link}}</i></li></ul></footer>",
+        //templateUrl:'template.html',
+        restrict:"E",
+        scope:{
+            links:"=socialProfiles"
+        },
+        random:{},
+        link: function(scope,element,attrs,controller){
+            
+        },
+        controller: function($scope,$element,$attrs){
+            console.log($scope.links);
+            var links = [];
+            var supportedMedia = {facebook:"fa fa-facebook",twitter:'fa fa-twitter'};
+            
+            $scope.links.forEach(function(link){
+                for(var media in link){
+                    if(supportedMedia[media]){
+                    var temp = {};
+                    temp.href = link[media];
+                    temp.class = supportedMedia[media];
+                    links.push(temp);
+                    }
+                }
+                $scope.links = links;
+                //console.log(link.class);
+            })
+        }
+        //controller:"",
+        //controllerAs:""
+    }
 });
